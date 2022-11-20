c2c536368f48521f146b898a1afcc15399e919a2
Max Fedak <max.fedak@gmail.com>
2022-11-21 01:41:33 +0200

    input validation

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -29,39 +29,42 @@ export const Input = ({ addItem, list}) => {
                addItem(newProduct);
                console.log(event)
             }
+
+            const allInputs = [...document.querySelectorAll('input')];
+            allInputs.map(item => item.value = '')
+         } else {
+            alert('Please fill all required fields *')go
          }
-      const allInputs = [...document.querySelectorAll('input')];
-      allInputs.map(item => item.value = '')
+
    }
 
-      // addItem(data)
-      return (
-         <form className='input' onSubmit={onSubmitHandler}>
-            <input 
-               type="text" 
-               name="name" 
-               placeholder='Product' 
-               className='input__field input__field--text'
-               onChange={onFormChangeHandler}
-            ></input>
-            <input 
-               type="number" 
-               name="quantity" 
-               placeholder='Quant.'
-               className='input__field input__field--number'
-               onChange={onFormChangeHandler}
-            ></input>
-            <input
-               type="number" 
-               name="price" 
-               placeholder='Price' 
-               step={0.01}
-               className='input__field input__field--number'
-               onChange={onFormChangeHandler}></input>
-            <button className='input__button' type='submit'>
-               <FontAwesomeIcon className='input__delete-icon' icon={faPlus} rotation={35} />
-            </button>
-         </form>
-      )
+   return (
+      <form className='input' onSubmit={onSubmitHandler}>
+         <input 
+            type="text" 
+            name="name" 
+            placeholder='Product*' 
+            className='input__field input__field--text'
+            onChange={onFormChangeHandler}
+         ></input>
+         <input 
+            type="number" 
+            name="quantity" 
+            placeholder='Quant.'
+            className='input__field input__field--number'
+            onChange={onFormChangeHandler}
+         ></input>
+         <input
+            type="number" 
+            name="price" 
+            placeholder='Price*' 
+            step={0.01}
+            className='input__field input__field--number'
+            onChange={onFormChangeHandler}></input>
+         <button className='input__button' type='submit'>
+            <FontAwesomeIcon className='input__delete-icon' icon={faPlus} rotation={35} />
+         </button>
+      </form>
+   )
 }
 
