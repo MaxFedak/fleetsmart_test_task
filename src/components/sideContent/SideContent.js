98350142c84145f65eded610ae3975263a7b721a
Max Fedak <max.fedak@gmail.com>
2022-11-22 11:38:58 +0200

    reset button added

--- src/components/sideContent/SideContent.js
+++ src/components/sideContent/SideContent.js
@@ -6,12 +6,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export const SideContent = ({itemList, addItemsToArray}) => {
+   const discrardClickHandler = (e) => {
+      addItemsToArray([]);
+   }
    return (
       <div className='side-content'>
          <h1>
             <FontAwesomeIcon icon={faCartShopping} />
             {' Product card'}
          </h1>
+         <button className='side-content__discard' onClick={discrardClickHandler}>
+            Discard all
+         </button>
          <Input addItem={addItemsToArray} list={itemList}/>
          <TotalPrice list={itemList} />
       </div>
